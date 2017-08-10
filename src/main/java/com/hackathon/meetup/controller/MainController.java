e90f4ba2a899969a4997824928bba2e1e3316180
DavidTurkGT <dlturk747@gmail.com>
2017-08-09 22:46:10 -0400

    Added event table and wiring

--- src/main/java/com/hackathon/meetup/controller/MainController.java
+++ src/main/java/com/hackathon/meetup/controller/MainController.java
@@ -1,15 +1,42 @@
 package com.hackathon.meetup.controller;
 
+import com.hackathon.meetup.domain.Event;
+import com.hackathon.meetup.domain.Status;
+import com.hackathon.meetup.domain.User;
+import com.hackathon.meetup.repository.EventRepo;
 import com.hackathon.meetup.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.List;
+
 @RestController
 public class MainController {
 
     @Autowired
-    UserRepo userRepo;
+    UserRepo users;
+
+    @Autowired
+    EventRepo events;
+
+    @PostConstruct
+    public void init(){
+        if(users.count() == 0){
+            User testUser = new User("David","Turk","dtgt","pass","test@test.org","8675309",true);
+            users.save(testUser);
+        }
+        User testUser = users.findAll().get(0);
+        System.out.println("User: " + testUser);
+        if(events.count() == 0){
+            Event newEvent = new Event(testUser, "Test Event", "Moon","Creating things", new Date(), Status.NEW);
+            events.save(newEvent);
+        }
+        Event testEvent = events.findAll().get(0);
+        System.out.println("Event: " + testEvent);
+    }
 
    
     @GetMapping("/")
